8bc3d0f69688ee9e82dcac3fba83ade849459f7e
Devmdmainuddin <devmdmainuddin@gmail.com>
2024-06-01 01:32:24 +0600

    create project

---
+++ index.js
@@ -0,0 +1,112 @@
+const express = require('express')
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion ,ObjectId} = require('mongodb');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 5000
+
+
+
+app.use(cors({
+    origin: ['http://localhost:5173',
+      'http://localhost:5174'
+    ],
+    credentials: true
+  }));
+  app.use(express.json());
+  app.use(cookieParser());
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.sk1ew0y.mongodb.net/?retryWrites=true&w=majority&appName=cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("education-elite").collection("users")
+
+  // auth related api
+  app.post('/jwt', async (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCRSS_TOKEN_SECRET, { expiresIn: '1h' })
+    res.send({ token })
+  })
+  // Logout
+  app.post('/logout', async (req, res) => {
+    const user = req.body;
+ 
+    res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+})
+
+    // ...............................users...................................
+
+
+app.put('/user',async(req,res)=>{
+    const user = req.body
+    const query = {email:user?.email}
+  const isExist = await userCollection.findOne(query)
+  if(isExist){
+    if(user.status === 'Requested'){
+      const result = await userCollection.updateOne(query,{
+        $set:{status:user?.status},
+      })
+      return res.send(result)
+    }else{
+      return res.send(isExist)
+    }
+  }
+  
+    const options = {upsert:true}
+   
+    const updateDoc ={
+      $set:{
+        ...user,
+        Timestamp: Date.now(),
+      },
+    }
+    const result = await userCollection.updateOne(query,updateDoc,options)
+    res.send(result)
+  })
+
+  app.patch('/users/update/:email',async(req,res)=>{
+    const email = req.params.email
+    const user = req.body
+    const query = {email}
+    const updateDoc ={
+      $set:{...user,Timestamp: Date.now()},
+  
+    }
+    const result = await userCollection.updateOne(query,updateDoc)
+    res.send(result)
+  })
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
