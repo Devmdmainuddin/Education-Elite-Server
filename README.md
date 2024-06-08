5b51e900faa6a46c6b0829773963c6d91554eca7
Devmdmainuddin <devmdmainuddin@gmail.com>
2024-06-08 22:09:06 +0600

    readme.md file create

---
+++ README.md
@@ -0,0 +1,66 @@
+
+# Education-Elite
+
+it's a AScholarship Management System website
+where student can search there suitable university and Scholarship also they can
+apply for the scholarship through our Scholarship Management System.
+
+
+
+## Installation
+
+Install my-project with npm
+
+```bash
+  git clone my project
+  cd my-project
+  npm i
+  npm run dev
+```
+    
+## website live link 
+
+
+
+[https://education-elite-e.web.app/](https://education-elite-e.web.app/)
+
+
+## Features
+
+
+- 3 type of dashboard
+- admin can Scholarship Add , Manage ,delete 
+-  admin can Manage Applied Application
+- admin Will see All Review , delete Review
+- admin will see all the user information , and manage user role 
+- user can Applied  in any scholarship 
+- user dashboard user can see only his/her all applied scholarship
+that he/she already applied
+-  Scholarship Descending order by sort
+- all Scholarship 
+- Testimonials
+- user profile
+- update user profile
+
+## Appendix
+
+This project is used by the following  
+
+- mongodb database
+- axios
+- react-date-range
+- react-helmet-async
+- react-hook-form
+- react-icons
+- react-toastify
+- react-hot-toast
+- react-rating
+- recharts
+- sort-by
+- sweetalert2
+- swiper
+
+
+## Authors
+
+- [@devmdmainuddin](https://github.com/Devmdmainuddin)
--- index.js
+++ index.js
@@ -12,12 +12,21 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors({
   origin: ['http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'https://education-elite-e.web.app',
+    'education-elite-e.firebaseapp.com'
   ],
   credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
+
+
+
+
+
+
+
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.sk1ew0y.mongodb.net/?retryWrites=true&w=majority&appName=cluster0`;
 
@@ -39,12 +48,51 @@ async function run() {
     const appliedScholarshipCollection = client.db("education-elite").collection("apply")
     const contactUSCollection = client.db("education-elite").collection("contactUS")
 
+
+
+
     // auth related api
     app.post('/jwt', async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCRSS_TOKEN_SECRET, { expiresIn: '1h' })
       res.send({ token })
     })
+
+
+  // middlewares 
+
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access,1' });
+      }
+      const data = req.headers.authorization.split(' ');
+      const token = data[1]
+      jwt.verify(token, process.env.ACCRSS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access ,2' })
+        }
+        req.decoded = decoded;
+
+        next();
+      })
+    }
+
+
+
+  // use verify admin after verifyToken
+  const verifyAdmin = async (req, res, next) => {
+    const email = req.decoded.email;
+    const query = { email: email };
+    const user = await userCollection.findOne(query);
+    const isAdmin = user?.role === 'admin';
+    if (!isAdmin) {
+      return res.status(403).send({ message: 'forbidden access,3' });
+    }
+    next();
+  }
+
+
     // Logout
     app.post('/logout', async (req, res) => {
       const user = req.body;
@@ -52,18 +100,10 @@ async function run() {
       res.clearCookie('token', { maxAge: 0 }).send({ success: true })
     })
     // ......................................................
-    //......................all scholarship........................
-    // app.get('/allScholarShip', async (req, res) => {
-    //   const result = await ScholarShipCollection.find().toArray()
-    //   res.send(result)
-    // })
+
 
     app.get('/ScholarShip', async (req, res) => {
-      const result = await ScholarShipCollection.find().
-        sort(
-          { postDate: -1 },
-          { ApplicationFees: -1 }
-        ).toArray();
+      const result = await ScholarShipCollection.find().sort({ postDate: -1 }).toArray();
       res.send(result)
     })
     //................details......................
@@ -79,10 +119,10 @@ async function run() {
 
     //.................................................................
 
-    app.get('/ScholarShips', async (req, res) => {
-      const result = await ScholarShipCollection.find().toArray()
-      res.send(result)
-    })
+    // app.get('/ScholarShips', async (req, res) => {
+    //   const result = await ScholarShipCollection.find().toArray()
+    //   res.send(result)
+    // })
 
 
     app.get('/ScholarShips/:id', async (req, res) => {
@@ -96,19 +136,6 @@ async function run() {
       };
       const result = await ScholarShipCollection.findOne(query, options)
       res.send(result);
-    })
-
-    // .............................................
-    app.get('/filteruser', async (req, res) => {
-      const filter = req.query.filter
-      const sort = req.query.sort
-      console.log('pagination query',req.query)
-      let query ={}
-      if (filter) query.role = filter
-      let options = {}
-      if (sort) options = { sort: { Timestamp: sort === 'asc' ? 1 : -1 } }
-      const result = await userCollection.find(query, options).toArray()
-      res.send(result)
     })
 
 
@@ -182,7 +209,12 @@ async function run() {
 
 
     app.get('/applyScholarShip', async (req, res) => {
-      const result = await appliedScholarshipCollection.find().toArray()
+      const sort = req.query.sort
+      console.log(sort);
+      let options = {}
+      if (sort) options = { sort: { applyDate: sort === 'asc' ? 1 : -1 } }
+
+      const result = await appliedScholarshipCollection.find(options).toArray()
       res.send(result)
     })
 
@@ -204,6 +236,7 @@ async function run() {
       const result = await appliedScholarshipCollection.find(query).toArray()
       res.send(result)
     })
+
     // ....................... delete application info..................................
     app.delete('/applyScholarShip/:id', async (req, res) => {
       const id = req.params.id
@@ -269,25 +302,38 @@ async function run() {
     })
 
     //..................... review show..........................
+   //..........................................................................
 
+
+    app.get('/reviews/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { 'sholarshipId': id }
+      const result = await reviewsCollection.find(query).sort({ reviewDate: -1 }).toArray()
+      res.send(result)
+    })
 
     app.get('/reviews', async (req, res) => {
-      const result = await reviewsCollection.find().toArray()
+      const result = await reviewsCollection.find().sort({ reviewDate: -1 }).toArray()
       res.send(result)
     })
     //..........................................................................
+    // app.get('/applyScholarShip/:email', async (req, res) => {
+    //   const email = req.params.email
+    //   const query = { 'userEmail': email }
+    //   const result = await appliedScholarshipCollection.find(query).toArray()
+    //   res.send(result)
+    // })
 
-
-    app.get('/reviews/:email', async (req, res) => {
+    app.get('/review/:email',verifyToken, async (req, res) => {
       const email = req.params.email
-      const query = { 'reviewerEmail': email }
+      const query = { 'reviewerEmail':email }
       const result = await reviewsCollection.find(query).toArray()
       res.send(result)
     })
 
     //..........................................................................
 
-    app.delete('/reviews/:id', async (req, res) => {
+    app.delete('/reviews/:id',verifyToken, async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await reviewsCollection.deleteOne(query)
@@ -296,7 +342,7 @@ async function run() {
 
     //..........................................................................
     // update  reviews in db
-    app.put('/reviews/:id', async (req, res) => {
+    app.put('/reviews/:id',verifyToken, async (req, res) => {
       const id = req.params.id
       const review = req.body
       const query = { _id: new ObjectId(id) }
@@ -346,18 +392,46 @@ async function run() {
       const result = await userCollection.updateOne(query, updateDoc, options)
       res.send(result)
     })
+
     app.get('/user/:email', async (req, res) => {
       const email = req.params.email
       const result = await userCollection.findOne({ email })
       res.send(result)
     })
+    
+    // .............................................
+    app.get('/filteruser',verifyToken, verifyAdmin, async (req, res) => {
+      const filter = req.query.filter
+      const sort = req.query.sort
+      // console.log('pagination query',req.query)
+      let query ={}
+      if (filter) query.role = filter
+      let options = {}
+      if (sort) options = { sort: { Timestamp: sort === 'asc' ? 1 : -1 } }
+      const result = await userCollection.find(query, options).toArray()
+      res.send(result)
+    })
+
     // ...........................
-    app.get('/users', async (req, res) => {
+    app.get('/users',verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray()
       res.send(result)
     })
 
-    app.patch('/users/update/:email', async (req, res) => {
+    app.patch('/users/:email',verifyToken, async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      console.log(req.body);
+      const query = { email }
+      const updateDoc = {
+        $set: { ...user, Timestamp: Date.now() },
+      }
+      const result = await userCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+
+
+    app.patch('/users/update/:email',verifyToken,verifyAdmin, async (req, res) => {
       const email = req.params.email
       const user = req.body
       const query = { email }
@@ -368,7 +442,7 @@ async function run() {
       const result = await userCollection.updateOne(query, updateDoc)
       res.send(result)
     })
-    app.delete('/users/:id', async (req, res) => {
+    app.delete('/users/:id',verifyToken,verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await userCollection.deleteOne(query);
@@ -376,7 +450,7 @@ async function run() {
     })
 
     // .......................payments..........................
-    app.post("/create-payment-intent", async (req, res) => {
+    app.post("/create-payment-intent",verifyToken, async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100)
       const paymentIntent = await stripe.paymentIntents.create({
@@ -391,16 +465,7 @@ async function run() {
       });
     });
 
-    // app.get('/payments/:email',verifyToken,async(req,res)=>{
-    //   const query = {email:req.params.email}
-    //   if(req.params.email !== req.decoded.email){
-    //     return res.status(403).send({message:'forbidden access'})
-    //   }
-    //   const result = await paymentCollection.find(query).toArray();
-    //   res.send(result)
-    // })
-
-    app.post('/payments', async (req, res) => {
+    app.post('/payments',verifyToken, async (req, res) => {
       const payment = req.body
       const result = await paymentCollection.insertOne(payment)
       // console.log('payments info',payment);
@@ -412,6 +477,132 @@ async function run() {
       // const deleteResult = await paymentCollection.deleteMany(query)
       res.send({ result })
     })
+
+    app.get('/admin-stats',async(req,res)=>{
+      const users = await userCollection.estimatedDocumentCount();
+      const apply = await appliedScholarshipCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+      const result = await paymentCollection.aggregate([
+        {
+          $group:{
+            _id: null,
+            totalRevenue:{
+              $sum:'$price'
+            }
+          }
+        }
+      ]).toArray()
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+      res.send({users,apply,orders,revenue})
+    })
+    
+
+
+  app.get('/payment-stats', async(req, res) =>{
+    const result = await paymentCollection.aggregate([
+      {
+        $unwind: '$cartIds'
+      },
+    {
+      $addFields: {
+        cartIds: { $toObjectId: "$cartIds" }
+      }
+    },
+      {
+        $lookup: {
+          from: 'ScholarShip',
+          localField: 'cartIds',
+          foreignField: '_id',
+          as: 'cartitems'
+        }
+      },
+      {
+        $unwind:'$cartitems'
+      },
+      
+      {
+        $addFields: {
+          "cartitems.TuitionFees": {
+            $cond: {
+              if: { $isNumber: "$cartitems.TuitionFees" },
+              then: "$cartitems.TuitionFees",
+              else: { $toDouble: "$cartitems.TuitionFees" }
+            }
+          }
+        }
+      },
+      {
+        $addFields: {
+          "cartitems.ApplicationFees": {
+            $cond: {
+              if: { $isNumber: "$cartitems.ApplicationFees" },
+              then: "$cartitems.ApplicationFees",
+              else: { $toDouble: "$cartitems.ApplicationFees" }
+            }
+          }
+        }
+      },
+      {
+        $addFields: {
+          "cartitems.ServiceCharge": {
+            $cond: {
+              if: { $isNumber: "$cartitems.ServiceCharge" },
+              then: "$cartitems.ServiceCharge",
+              else: { $toDouble: "$cartitems.ServiceCharge" }
+            }
+          }
+        }
+      },
+
+      {
+        $match: {
+          "cartitems.TuitionFees": { $ne: null }
+        }
+      },
+      {
+        $match: {
+          "cartitems.ApplicationFees": { $ne: null }
+        }
+      },
+      {
+        $match: {
+          "cartitems.ServiceCharge": { $ne: null }
+        }
+      },
+
+      {
+        $group: {
+          _id: '$cartitems.SubjectCategorey',
+          quantity:{ $sum: 1 },
+          Revenue:{
+            $sum:{
+              $add:[
+                '$cartitems.TuitionFees',
+                '$cartitems.ServiceCharge',
+                '$cartitems.TuitionFees'
+              ]
+            }
+          }  
+        }
+      },   
+   
+    {
+      $project: {
+        _id: 0,
+        category: "$_id",
+        quantity:'$quantity',
+        Revenue: '$Revenue'
+      }
+    }
+    ]).toArray();
+
+    res.send(result);
+
+  })
+
+
+
+
 
 
     // await client.db("admin").command({ ping: 1 });
